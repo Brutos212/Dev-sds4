06ee4b1d31aab4b2fbed8c4d3a68eb052d2e734f
Brutos212 <fabio212@hotmail.com>
2021-09-10 21:33:03 -0300

    Basic static components

---
+++ sd4-front/src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">App desenvolvido por <a href="https://github.com/Brutos212 " target="_blank" rel="noreferrer">Fabio Santos</a></p>
+                    <p className="text-light"><small><strong>Semana de estudo com Spring React</strong><br />
+                        Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                </div>
+            </footer>
+
+        </div>
+    );
+}
+
+export default Footer;
